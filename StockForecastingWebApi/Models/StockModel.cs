11b67858e01469a5b2948a0d0d014dd87734ac4b
Andrey <androrvit@gmail.com>
2022-12-05 16:52:48 +0300

    add props in StockModel

--- StockForecastingWebApi/Models/StockModel.cs
+++ StockForecastingWebApi/Models/StockModel.cs
@@ -2,5 +2,13 @@
 {
     public class StockModel
     {
+        public StockModel(DateTime stockDate, int closingPrice)
+        {
+            StockDate=stockDate;
+            ClosingPrice=closingPrice;
+        }
+
+        public DateTime StockDate { get; set; }
+        public int ClosingPrice { get; set; }
     }
 }
