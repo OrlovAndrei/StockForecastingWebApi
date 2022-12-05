13a362e14e68346958b67de72e1ae354ea6a5486
Andrey <androrvit@gmail.com>
2022-12-05 16:48:03 +0300

    add work with yahoo api

--- StockForecastingWebApi/Services/DataFetcher.cs
+++ StockForecastingWebApi/Services/DataFetcher.cs
@@ -1,6 +1,27 @@
-﻿namespace StockForecastingWebApi.Services
+﻿using StockForecastingWebApi.Models;
+using YahooFinanceApi;
+
+namespace StockForecastingWebApi.Services
 {
-    public class DataFetcher
+    public static class DataFetcher
     {
+        public static async Task<IEnumerable<StockModel>> GetHistoricalData(string symbol)
+        {
+            var stockData = new List<StockModel>();
+            try
+            {
+                var historicalData = await Yahoo.GetHistoricalAsync(symbol, new DateTime(2015, 1, 1), DateTime.Now, Period.Daily);
+                foreach(var item in historicalData)
+                {
+                    stockData.Add(new StockModel());
+                }
+                return stockData;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
     }
 }
