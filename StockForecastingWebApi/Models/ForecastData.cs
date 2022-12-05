4ce572b3424e1135d5f5133d76f25bccac5af630
Andrey <androrvit@gmail.com>
2022-12-05 20:14:21 +0300

    add ForecastData

---
+++ StockForecastingWebApi/Models/ForecastData.cs
@@ -0,0 +1,11 @@
+﻿namespace StockForecastingWebApi.Models
+{
+    public class ForecastData
+    {
+        public float[] ForecastedRentals { get; set; }
+
+        public float[] LowerBoundRentals { get; set; }
+
+        public float[] UpperBoundRentals { get; set; }
+    }
+}
