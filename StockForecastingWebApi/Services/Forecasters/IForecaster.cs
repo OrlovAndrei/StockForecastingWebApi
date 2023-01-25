d7a0f91a0ea3a1c1547cbf0d52f45606c34c2e75
Andrey <androrvit@gmail.com>
2023-01-25 14:57:46 +0300

    add IForecaster

---
+++ StockForecastingWebApi/Services/Forecasters/IForecaster.cs
@@ -0,0 +1,9 @@
+﻿using StockForecastingWebApi.Models;
+
+namespace StockForecastingWebApi.Services
+{
+    public interface IForecaster
+    {
+        public ForecastData Forecast(List<Stock> historicData);
+    }
+}
