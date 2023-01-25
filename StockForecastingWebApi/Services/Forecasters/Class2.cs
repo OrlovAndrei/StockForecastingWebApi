d3187c3c7f2f3472579980a2957836f634c0aa96
Andrey <androrvit@gmail.com>
2023-01-25 15:15:29 +0300

    add forecaster classes

---
+++ StockForecastingWebApi/Services/Forecasters/Class2.cs
@@ -0,0 +1,13 @@
+﻿using StockForecastingWebApi.Models;
+
+namespace StockForecastingWebApi.Services
+{
+    [Forecaster("C2")]
+    public class Class2 : IForecaster
+    {
+        public ForecastData Forecast(List<Stock> historicData)
+        {
+            return new ForecastData();
+        }
+    }
+}
---
+++ StockForecastingWebApi/Services/Forecasters/ForecasterAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace StockForecastingWebApi.Services
+{
+    public class ForecasterAttribute : Attribute
+    {
+        public string Name { get; set; }
+
+        public ForecasterAttribute(string name)
+        {
+            Name=name;
+        }
+    }
+}
